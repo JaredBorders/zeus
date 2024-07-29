83b7ea248d55b70aa7d654e0dbc7494365fd4c1a
JaredBorders <jaredborders@pm.me>
2024-07-28 23:48:50 -0400

    ✅ add order tests

--- test/test.ml
+++ test/test.ml
@@ -1,15 +1,21 @@
 open Zeus.Order
 
-let%test "order create" =
-  let order = Order.create 1 GoodTillCancel Buy 100.0 100 in
-  order.id = 1
-  && order.kind = GoodTillCancel
-  && order.side = Buy
-  && order.price = 100.0
-  && order.quantity = 100
-;;
+module OrderTests = struct
+  let%test "order create bid" =
+    let order = Order.create 1 Limit Bid 100.0 100 in
+    order.id = 1
+    && order.kind = Limit
+    && order.side = Bid
+    && order.price = 100.0
+    && order.quantity = 100
+  ;;
 
-let%test "order string_of_order" =
-  let order = Order.create 2 Market Sell 190.23 999 in
-  Order.string_of_order order = "Order 2: Market Sell 190.230000 999"
-;;
+  let%test "order create ask" =
+    let order = Order.create 1 Limit Ask 100.0 100 in
+    order.id = 1
+    && order.kind = Limit
+    && order.side = Ask
+    && order.price = 100.0
+    && order.quantity = 100
+  ;;
+end
