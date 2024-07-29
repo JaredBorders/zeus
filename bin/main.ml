6f3254e550829744f74c900babd8b9905009b643
JaredBorders <jaredborders@pm.me>
2024-07-28 23:59:00 -0400

    ⚡️ add order creation and printing

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,9 @@
-let () = print_endline "<><> zeus.0.0.1 ><><><><><><><><><><><><><><><><>"
+open Zeus.Order
+
+let order = Order.create 1 Market Bid 100.0 100
+
+let () =
+  print_endline "\n<><> zeus.0.0.1 ><><><><><><><><><><><><><><><><>\n";
+  print_endline (Order.json order);
+  print_endline "\n"
+;;
