eb8a41bc11048dc45a411fcbb16603642c6f061b
JaredBorders <jaredborders@pm.me>
2024-07-28 23:56:40 -0400

    👷🏻‍♂️ add yojson

--- lib/order.ml
+++ lib/order.ml
@@ -1,12 +1,16 @@
+open Ppx_yojson_conv_lib.Yojson_conv.Primitives
+
 module Order = struct
   type kind =
     | Market
     | Limit
     | Stop
+  [@@deriving yojson]
 
   type side =
     | Bid
     | Ask
+  [@@deriving yojson]
 
   type t =
     { id : int
@@ -15,7 +19,9 @@ module Order = struct
     ; price : float
     ; quantity : int
     }
+  [@@deriving yojson]
 
   let create id kind side price quantity = { id; kind; side; price; quantity }
   let modify t kind side price quantity = { t with kind; side; price; quantity }
+  let json t = Yojson.Safe.to_string (yojson_of_t t)
 end
