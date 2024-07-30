73b6530dc8785b1ba57245071067bd0112386464
JaredBorders <jaredborders@pm.me>
2024-07-29 21:13:19 -0400

    👷🏻‍♂️ refactor order module

--- lib/order.ml
+++ lib/order.ml
@@ -19,11 +19,12 @@ module Order = struct
     }
 
   let create id kind side price quantity = { id; kind; side; price; quantity }
-  let modify t kind side price quantity = { t with kind; side; price; quantity }
-  let filled t = t.quantity = 0
+  let modify t kind price quantity = { t with kind; price; quantity }
 
   let fill t quantity =
     if quantity > t.quantity then raise Invalid_quantity;
     { t with quantity = t.quantity - quantity }
   ;;
+
+  let filled t = t.quantity = 0
 end
