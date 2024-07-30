cbebe70f1f61709ca990ea1b025d827668aff565
JaredBorders <jaredborders@pm.me>
2024-07-29 21:19:30 -0400

    👷🏻‍♂️ add order to trade module

--- lib/trade.ml
+++ lib/trade.ml
@@ -1,14 +1,16 @@
+open Order
+
 module Trade = struct
-  type details =
-    { id : int
-    ; price : float
-    ; quantity : int
-    }
+  exception Invalid_bid
+  exception Invalid_ask
 
   type t =
-    { bid : details
-    ; ask : details
+    { bid : Order.t
+    ; ask : Order.t
     }
 
-  let create bid ask = { bid; ask }
+  let create (bid : Order.t) (ask : Order.t) = 
+    if bid.side <> Order.Bid then raise Invalid_bid;
+    if ask.side <> Order.Ask then raise Invalid_ask;
+    { bid; ask }
 end
