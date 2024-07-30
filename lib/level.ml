ff065c0bae965ef90d1c8e79d4aa3e9a0f0b8025
JaredBorders <jaredborders@pm.me>
2024-07-29 21:38:24 -0400

    🗑️ remove side from level

--- lib/level.ml
+++ lib/level.ml
@@ -9,17 +9,13 @@ module Level = struct
 
   type t =
     { price : float
-    ; side : Order.side
     ; quantity : int
     ; orders : Order.t OrderMap.t
     }
 
-  let create price (side : Order.side) =
-    { price; side; quantity = 0; orders = OrderMap.empty }
-  ;;
+  let create price = { price; quantity = 0; orders = OrderMap.empty }
 
   let add (order : Order.t) level =
-    if order.Order.side <> level.side then raise Wrong_side;
     if order.Order.price <> level.price then raise Wrong_price;
     let orders = OrderMap.add order.Order.id order level.orders in
     let quantity = level.quantity + order.Order.quantity in
